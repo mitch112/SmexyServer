ba273a76d4016e8fbe9687592702c87d9a686b02
felixx89 <felixx89@live.nl>
2012-12-08 23:45:51 +0100

    Update memory max

--- Nexon-World GameServer/~Run.bat
+++ Nexon-World GameServer/~Run.bat
@@ -1,4 +1,4 @@
 @echo off
 title Nexon-World (43594)
-java -client -Xmx1512m -cp bin;lib/* org.dementhium.RS2ServerBootstrap
+java -client -Xmx2028m -cp bin;lib/* org.dementhium.RS2ServerBootstrap
 pause
