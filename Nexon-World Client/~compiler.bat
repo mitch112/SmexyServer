930b01f48af02ebb2060296f738d1780df264887
felixx89 <felixx89@live.nl>
2012-12-10 20:41:20 +0100

    Java update
    Signed-off-by: felixx89 <felixx89@live.nl>

--- Nexon-World Client/~compiler.bat
+++ Nexon-World Client/~compiler.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files\Java\jdk1.7.0_07\bin\javac.exe" -cp lib/clientlibs.jar;lib; *.java
+"C:\Program Files\Java\jdk1.7.0_09\bin\javac.exe" -cp lib/clientlibs.jar;lib; *.java
 @pause
 
